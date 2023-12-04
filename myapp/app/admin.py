1b2bd2831f860b85ecc9592170e080773b8cf3b4
Алексей <alexey.maltsev02@mail.ru>
2023-12-04 11:31:54 +0700

    register models

--- myapp/app/admin.py
+++ myapp/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Player, Club, City, Country, Contract, Transfer
 
 # Register your models here.
+admin.site.register(Player)
+admin.site.register(City)
+admin.site.register(Country)
+admin.site.register(Contract)
+admin.site.register(Club)
+admin.site.register(Transfer)
