c8073ee9a2ff1324dfb77ea3712a5d38c9cd2e3d
Алексей <alexey.maltsev02@mail.ru>
2023-12-04 13:33:55 +0700

    add IndexView

--- myapp/app/views.py
+++ myapp/app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import View
+from .models import Player
 
 
-def index(request):
-    return HttpResponse("Hello, world.")
+class IndexView(View):
+    template_name = "app/index.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
