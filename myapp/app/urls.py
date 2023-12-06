e06faa802e96b1e0448ab0624044fef7f3a20946
Алексей <alexey.maltsev02@mail.ru>
2023-12-06 22:22:11 +0700

    add paths register & login

--- myapp/app/urls.py
+++ myapp/app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import IndexView
-
+from .views import IndexView, RegisterPage, LoginPage
 from . import views
+
+app_name = 'app'
 
 urlpatterns = [
     path("", IndexView.as_view(), name='index'),
+    path("register/", RegisterPage.as_view(), name='register'),
+    path("login/", LoginPage.as_view(), name='login'),
 ]
