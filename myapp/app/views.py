497506d1f5596e56f901b461e1abedc353fd497c
Алексей <alexey.maltsev02@mail.ru>
2023-12-06 21:54:01 +0700

    add standard register django

--- myapp/app/views.py
+++ myapp/app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
-from .models import Player
+from django.contrib.auth.forms import UserCreationForm
 
 
 class IndexView(View):
@@ -8,3 +8,28 @@ class IndexView(View):
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
+
+
+class RegisterPage(View):
+    template_name = 'app/register.html'
+
+    def get(self, request):
+        form = UserCreationForm()
+        context = {'form': form}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('app')  # Замените 'home' на имя вашей домашней страницы
+        context = {'form': form}
+        return render(request, self.template_name, context)
+
+
+class LoginPage(View):
+    template_name = "app/login.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
