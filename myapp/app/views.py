560ec2fdc9614a7637874fca25c0976691811f71
Алексей <alexey.maltsev02@mail.ru>
2023-12-12 14:54:55 +0700

    add login_required decorator

--- myapp/app/views.py
+++ myapp/app/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from .models import Player, Contract, Transfer
-
-from .forms import CreateUserForm
+from .forms import CreateUserForm, PlayerForm
 
 
 class IndexView(View):
@@ -17,18 +18,30 @@ class IndexView(View):
 class ProfileView(View):
     template_name = "app/profile.html"
 
+    @method_decorator(login_required(login_url='app:login'))
     def get(self, request, *args, **kwargs):
         players = Player.objects.all()  # Получаем все объекты из базы данных
         contracts = Contract.objects.all()
         transfers = Transfer.objects.all()
 
+        player_form = PlayerForm()
+
         context = {
             'players': players,
             'contracts': contracts,
             'transfers': transfers,
-            }
+            'player_form': player_form,
+        }
 
         return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        if 'player-form' in request.POST:
+            player_form = PlayerForm(request.POST)
+            if player_form.is_valid():
+                player_form.save()
+
+        return redirect('app:profile')
 
 
 class RegisterPage(View):
@@ -74,5 +87,3 @@ class LoginPage(View):
 def logout_user(request):
     logout(request)
     return redirect('app:login')
-
-
