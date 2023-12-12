c06e1ef80097f67e4c5d99d70b91e3dadc963181
Алексей <alexey.maltsev02@mail.ru>
2023-12-12 14:55:36 +0700

    add player_form

--- myapp/app/forms.py
+++ myapp/app/forms.py
@@ -1,9 +1,16 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Player, Contract, Transfer
 
 
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class PlayerForm(forms.ModelForm):
+    class Meta:
+        model = Player
+        fields = '__all__'
